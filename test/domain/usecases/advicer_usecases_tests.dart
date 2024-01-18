bdd744ec8a1a69c61ff033b2e2f7c88052f8b8ff
monkeytower <ok@monkeytower.net>
2024-01-18 23:59:04 +0100

    OK before digging into usecase tests chapter 65

---
+++ test/domain/usecases/advicer_usecases_tests.dart
@@ -0,0 +1,6 @@
+import 'package:mockito/annotations.dart';
+import 'package:advicer/domain/repositories/advicer_repository.dart';
+// import 'package:mockito/mockito.dart';
+
+@GenerateMocks([AdvicerRepository])
+void main() {}
